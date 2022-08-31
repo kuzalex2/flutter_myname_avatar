576cf3a860601e388fb967bb3a092b07ec0794a8
Alexey Kuznetsov <me@kuzalex.com>
2022-08-31 19:40:11 +0300

    DogApiClient

--- packages/dog_api/lib/dog_api.dart
+++ packages/dog_api/lib/dog_api.dart
@@ -1,7 +1,89 @@
 library dog_api;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+
+
+/// Exceptions
+class DogRequestNot200Failure implements Exception {}
+class DogRequestFailure implements Exception {}
+class DogRequestGeneralFailure implements Exception {}
+class DogRequestNoData implements Exception {}
+class DogRequestInvalidData implements Exception {}
+
+
+
+
+///
+/// Dart API Client which wraps the [Dog API](https://dog.ceo/dog-api/documentation/).
+///
+///
+
+class DogApiClient {
+
+
+  static final BaseOptions _options = BaseOptions(
+      baseUrl: 'https://dog.ceo',
+      connectTimeout: 5000,
+      receiveTimeout: 3000,
+    );
+  final Dio dio;
+
+  DogApiClient():
+        dio = Dio(_options)
+  ;
+
+
+
+
+  /// Returns an array of all the images from a breed, e.g. hound
+  Future<List<String>> breed(String breed) async {
+    final Response response;
+
+    try {
+      response = await dio.request('/api/breed/$breed/images');
+      debugPrint('$response');
+    } on DioError catch (e) {
+
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx and is also not 304.
+      if (e.response != null) {
+        debugPrint('{$e.response.data}');
+        throw DogRequestNot200Failure();
+
+      } else {
+
+        debugPrint('{$e.message}');
+        throw DogRequestFailure();
+      }
+    } catch (e) {
+      debugPrint('$e');
+
+      throw DogRequestGeneralFailure();
+    }
+
+    if (response.data == null) {
+      throw DogRequestNoData();
+    }
+    if (response.data["status"] != 'success') {
+      throw DogRequestInvalidData();
+    }
+
+    if (response.data["message"] is List<dynamic>){
+      final list = response.data["message"] as List<dynamic>;
+      if (list.where((element) => element is! String).isNotEmpty) {
+        throw DogRequestInvalidData();
+      }
+
+
+      return list.map((e) => e as String).toList();
+    } else {
+
+      throw DogRequestInvalidData();
+    }
+
+  }
+
+
 }
+
--- packages/dog_api/test/dog_api_test.dart
+++ packages/dog_api/test/dog_api_test.dart
@@ -2,11 +2,38 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:dog_api/dog_api.dart';
 
+
+
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group('DogApiClient', () {
+    late DogApiClient dogApiClient;
+
+
+    setUp(() {
+      dogApiClient = DogApiClient();
+    });
+
+    group('constructor', () {
+      test('default constructor', () {
+        expect(DogApiClient(), isNotNull);
+      });
+    });
+
+    group('breed', () {
+      test('non-existing breed', () async {
+        expect(
+              () async => dogApiClient.breed('somebreed'),
+          throwsA(isA<DogRequestNot200Failure>()),
+        );
+      });
+      test('existing breed', () async {
+        expect(
+          await dogApiClient.breed('hound'),
+          isA<List<String>>(),
+        );
+      });
+    });
+
+
   });
 }
