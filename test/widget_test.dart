8fca142bbef4297fe1da661279b9feaab5e18565
Alexey Kuznetsov <me@kuzalex.com>
2022-09-01 13:24:49 +0300

    bloc test sample

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,90 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dog_api/dog_api.dart';
+import 'package:flutter_myname_avatar/bloc/person_name_cubit/person_name_cubit.dart';
+import 'package:flutter_myname_avatar/repository/repository.dart';
+import 'package:flutter_myname_avatar/repository/store_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:nationalize_api/nationalize_api.dart';
 
-import 'package:flutter_myname_avatar/main.dart';
+
+
+class StoreRepositoryMock extends Mock implements StoreRepository {
+
+}
+
+class DogApiClientMock extends Mock implements DogApiClient {
+
+}
+
+class NationalizeApiClientMock extends Mock implements NationalizeApiClient {
+
+}
+
+class MockRepo extends Mock implements Repository {
+
+  late final DogApiClient dogApiClient ;
+  late final NationalizeApiClient nationalizeApiClient;
+  late final StoreRepository storeRepository;
+
+
+  @override
+  init() async {
+    dogApiClient = DogApiClient();
+    nationalizeApiClient = NationalizeApiClient();
+
+    storeRepository = StoreRepository(db: SaveLoadMock());
+    await storeRepository.init();
+
+  }
+}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  group('AppCubit', () {
+    late PersonNameCubit cubit ;
+    late MockRepo repository;
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+
+    //
+    setUp(() async {
+      repository = MockRepo();
+      await repository.init();
+      cubit = PersonNameCubit(repository, nickName: '');
+    });
+
+
+
+    group('test1', () {
+      blocTest<PersonNameCubit, PersonNameState>(
+        'canSave test',
+        build: () => cubit,
+        act: (cubit) => cubit.nickNameChanged("al"),
+        wait: const Duration(milliseconds: 1000),
+        expect: () => <dynamic>[
+          isA<PersonNameState>()
+              .having((s) => s.canSave, 'canSave', false),
+
+        ],
+      );
+
+      blocTest<PersonNameCubit, PersonNameState>(
+        'nickNameStatus.isValid test',
+        build: () => cubit,
+        act: (cubit) => cubit.nickNameChanged("alex"),
+        wait: const Duration(milliseconds: 100),
+        expect: () => <dynamic>[
+          isA<PersonNameState>()
+              .having((s) => s.nickNameStatus.isValid, 'nickNameStatus.isValid', true),
+          isA<PersonNameState>()
+              .having((s) => s.nickNameStatus.isValid, 'nickNameStatus.isValid', true),
+
+        ],
+     );
+
+    });
+
+
   });
 }
